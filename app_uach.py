d14bacf7d460e379ef3199200b6791df75b3f1b2
Valeria Moreno <91974128+valmoreno28@users.noreply.github.com>
2022-03-27 19:46:20 -0700

    Add files via upload

---
+++ app_uach.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+
+st.write("""
+### ¡Hola! Bienvenido (a) a  *Nombre de la app*
+"Esta app te permitirá conocer algunos síntomas relacionados a alguna enfermedad de transmisión sexual
+""")
+
+st.write("Here's our first attemp at using data to create a table:")
+st.write(pd.DataFrame({
+	'first column': [1, 2, 3, 4],
+	'second column': [10, 20, 30, 40]
+	}))
+
+
+
+df=pd.DataFrame({
+	'first column': [1, 2, 3, 4],
+	'second column': [10, 20, 30, 40]
+	})
+
+option=st.selectbox(
+	'Which number do you like best?',
+	df ['first column']
+	
+'You selected:', option)
